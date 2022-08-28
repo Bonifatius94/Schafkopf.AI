180675d7317011e192dcdd15e1f24d98f39372ce
Marco Tröster <marco@troester-gmbh.de>
2022-08-28 10:26:30 +0200

    add game cost logic

--- Schafkopf.Lib/GameSession.cs
+++ Schafkopf.Lib/GameSession.cs
@@ -4,12 +4,14 @@ public class GameHistory
 {
     public GameHistory(GameCall call)
     {
-        this.call = call;
+        Call = call;
         eval = new TurnEvaluator(call);
     }
 
-    public GameCall call;
+    public GameCall Call { get; private set; }
     private readonly TurnEvaluator eval;
+
+    #region Turns
 
     private List<Turn> turns = new List<Turn>();
     public IReadOnlyList<Turn> Turns => turns;
@@ -17,26 +19,84 @@ public class GameHistory
     public void Append(Turn turn)
         => turns.Add(turn);
 
+    #endregion Turns
+
     #region Score
 
     private Dictionary<int, int> scoreByPlayer
         => turns.GroupBy(t => eval.WinnerId(t))
             .ToDictionary(g => g.Key, g => g.Select(x => x.Augen).Sum());
 
-    private IEnumerable<int> callerIds
-        => call.Mode == GameMode.Sauspiel
-            ? new List<int>() { call.CallingPlayerId, call.PartnerPlayerId }
-            : new List<int>() { call.CallingPlayerId };
+    public IEnumerable<int> CallerIds
+        => Call.Mode == GameMode.Sauspiel
+            ? new List<int>() { Call.CallingPlayerId, Call.PartnerPlayerId }
+            : new List<int>() { Call.CallingPlayerId };
 
-    private IEnumerable<int> opponentIds
-        => Enumerable.Range(0, 4).Except(callerIds).ToList();
+    public IEnumerable<int> OpponentIds
+        => Enumerable.Range(0, 4).Except(CallerIds).ToList();
 
-    public int ScoreCaller => callerIds.Select(x => scoreByPlayer[x]).Sum();
-    public int ScoreOpponents => opponentIds.Select(x => scoreByPlayer[x]).Sum();
-
-    // TODO: add conversion to a player-specific view (for simpler training)
+    public int ScoreCaller => CallerIds.Select(x => scoreByPlayer[x]).Sum();
+    public int ScoreOpponents => OpponentIds.Select(x => scoreByPlayer[x]).Sum();
 
     #endregion Score
+
+    public bool DidCallerWin => ScoreCaller > ScoreOpponents;
+    public bool IsSchneider => ScoreCaller > 90 || ScoreOpponents >= 90;
+    public bool IsSchwarz => Math.Abs(ScoreCaller - ScoreOpponents) == 120;
+
+    public GameResult ToPlayerResult(int playerId)
+        => new GameResult(this, playerId);
+}
+
+public class GameResult
+{
+    public GameResult(GameHistory history, int playerId)
+    {
+        this.playerId = playerId;
+        History = history;
+        Reward = computeReward(history, playerId);
+    }
+
+    private int playerId;
+    public GameHistory History { get; private set; }
+    public double Reward { get; private set; }
+
+    #region RewardComputation
+
+    private static readonly Dictionary<GameMode, double> baseChargeOfGame =
+        new Dictionary<GameMode, double>() {
+            { GameMode.Sauspiel, 10 },
+            { GameMode.Solo, 50 },
+            { GameMode.Wenz, 50 },
+        };
+    private const double chargePerLaufendem = 10;
+    private const double chargeSchneider = 10;
+    private const double chargeSchwarz = 10;
+
+    private static double computeReward(GameHistory history, int playerId)
+    {
+        int klopfer = 0; // TODO: include a game mechanism for player interaction
+        int laufende = 0; // TODO: find a way to compute this
+
+        double baseCharge = baseChargeOfGame[history.Call.Mode]
+            + (laufende * chargePerLaufendem)
+            + (history.IsSchneider ? chargeSchneider : 0)
+            + (history.IsSchwarz ? chargeSchwarz : 0);
+        double gameCost = baseCharge * (1 + klopfer);
+
+        bool isPlayer = history.CallerIds.Contains(playerId);
+        bool isOpponent = history.OpponentIds.Contains(playerId);
+        int numMates = isPlayer
+            ? history.CallerIds.Count()
+            : history.OpponentIds.Count();
+
+        bool isPlayerWinner =
+            (isPlayer && history.DidCallerWin) ||
+            (isOpponent && !history.DidCallerWin);
+        return (isPlayerWinner ? 1.0 : -1.0) * gameCost / numMates;
+    }
+
+    #endregion RewardComputation
 }
 
 public class GameSession
@@ -75,6 +135,7 @@ public class GameSession
             {
                 Card card;
 
+                // TODO: this seems to loop infinitely
                 while (true)
                 {
                     card = player.ChooseCard(turn);
