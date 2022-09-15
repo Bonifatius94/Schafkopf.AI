8dbdde3b79db1882c67e7191fe1b1b88e1949648
Marco Tröster <marco@troester-gmbh.de>
2022-09-15 20:44:41 +0200

    move trumpf eval into separate code file

--- Schafkopf.Lib/GameCall.cs
+++ Schafkopf.Lib/GameCall.cs
@@ -1,6 +1,3 @@
-using System.Runtime.Intrinsics;
-using System.Runtime.Intrinsics.X86;
-
 namespace Schafkopf.Lib;
 
 public enum GameMode
@@ -141,7 +138,7 @@ public class GameCallGenerator
             CardColor.Eichel
         };
 
-    private static readonly List<CardColor> soloColors =
+    private static readonly List<CardColor> soloTrumpf =
         new List<CardColor>() {
             CardColor.Schell,
             CardColor.Herz,
@@ -171,9 +168,8 @@ public class GameCallGenerator
                 color));
 
         var wenzen = touts.Select(tout => GameCall.Wenz(playerId, tout));
-
         var soli = touts.SelectMany(tout =>
-            soloColors.Select(c => GameCall.Solo(playerId, c, tout)));
+            soloTrumpf.Select(trumpf => GameCall.Solo(playerId, trumpf, tout)));
 
         var allGameCalls = sauspiele.Union(wenzen).Union(soli);
         var possibleCalls = last.Mode == GameMode.Weiter ? allGameCalls
@@ -187,96 +183,4 @@ public class GameCallGenerator
         return Enumerable.Range(0, 4)
             .First(i => initialHands[i].HasCard(gsuchteSau));
     }
-}
-
-public class TrumpfEval
-{
-    #region Masks
-
-    private const byte SIEBEN = (byte)((byte)CardType.Sieben << 2);
-    private const byte ACHT = (byte)((byte)CardType.Acht << 2);
-    private const byte NEUN = (byte)((byte)CardType.Neun << 2);
-    private const byte UNTER = (byte)((byte)CardType.Unter << 2);
-    private const byte OBER = (byte)((byte)CardType.Ober << 2);
-    private const byte KOENIG = (byte)((byte)CardType.Koenig << 2);
-    private const byte ZEHN = (byte)((byte)CardType.Zehn << 2);
-    private const byte SAU = (byte)((byte)CardType.Sau << 2);
-    private const byte SCHELL = (byte)CardColor.Schell;
-    private const byte HERZ = (byte)CardColor.Herz;
-    private const byte GRAS = (byte)CardColor.Gras;
-    private const byte EICHEL = (byte)CardColor.Eichel;
-
-    private static readonly Vector128<byte> WENZ_TRUMPF_CARDS =
-        Vector128.Create(
-            EICHEL | UNTER, GRAS | UNTER, HERZ | UNTER, SCHELL | UNTER,
-                      0xFF,         0xFF,         0xFF,           0xFF,
-                      0xFF,         0xFF,         0xFF,           0xFF,
-                      0xFF,         0xFF,         0xFF,           0xFF);
-
-    private static readonly Vector128<byte>[] SOLO_TRUMPF_CARDS =
-        new Vector128<byte>[] {
-            Vector128.Create(
-                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
-                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
-                SCHELL |   SAU, SCHELL |   ZEHN, SCHELL | KOENIG, SCHELL |  NEUN,
-                SCHELL |  ACHT, SCHELL | SIEBEN,            0xFF,           0xFF),
-            Vector128.Create(
-                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
-                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
-                  HERZ |   SAU,   HERZ |   ZEHN,   HERZ | KOENIG,   HERZ |  NEUN,
-                  HERZ |  ACHT,   HERZ | SIEBEN,            0xFF,           0xFF),
-            Vector128.Create(
-                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
-                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
-                  GRAS |   SAU,   GRAS |   ZEHN,   GRAS | KOENIG,   GRAS |  NEUN,
-                  GRAS |  ACHT,   GRAS | SIEBEN,            0xFF,           0xFF),
-            Vector128.Create(
-                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
-                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
-                EICHEL |   SAU, EICHEL |   ZEHN, EICHEL | KOENIG, EICHEL |  NEUN,
-                EICHEL |  ACHT, EICHEL | SIEBEN,            0xFF,           0xFF),
-        };
-
-    private static readonly Vector128<byte> ZERO =
-        Vector128.Create((byte)0);
-    private static readonly Vector128<byte> WENZ_MODE =
-        Vector128.Create((byte)GameMode.Wenz);
-
-    private static readonly Vector128<byte>[] MODE_MASKS =
-        new Vector128<byte>[] {
-            Vector128.Create((byte)GameMode.Weiter),
-            Vector128.Create((byte)GameMode.Sauspiel),
-            Vector128.Create((byte)GameMode.Wenz),
-            Vector128.Create((byte)GameMode.Solo),
-        };
-
-    #endregion Masks
-
-    public TrumpfEval(GameMode mode, CardColor trumpf = CardColor.Herz)
-    {
-        this.mode = mode;
-        this.trumpf = trumpf;
-    }
-
-    private readonly GameMode mode;
-    private readonly CardColor trumpf;
-
-    public bool IsTrumpf(Card card)
-    {
-        var cardVec = Vector128.Create((byte)(card.Id & 0x1F));
-        var modeMask = MODE_MASKS[(byte)mode];
-        var wenzModeMask = Sse2.CompareEqual(modeMask, WENZ_MODE);
-        var soloModeMask = Sse2.CompareEqual(wenzModeMask, ZERO);
-        var soloOrSauspielTrumpf = SOLO_TRUMPF_CARDS[(byte)trumpf];
-
-        var wenzCmp = Sse2.Xor(cardVec, WENZ_TRUMPF_CARDS);
-        var soloCmp = Sse2.Xor(cardVec, soloOrSauspielTrumpf);
-        var wenzMatches = Sse2.And(Sse2.CompareEqual(wenzCmp, ZERO), wenzModeMask);
-        var soloMatches = Sse2.And(Sse2.CompareEqual(soloCmp, ZERO), soloModeMask);
-        var allMatches = Sse2.Or(wenzMatches, soloMatches).AsUInt64();
-        bool isTrumpf = (allMatches.GetElement(0) | allMatches.GetElement(1)) > 0;
-        return isTrumpf;
-    }
-
-    // TODO: use this matching table to compute the rank for card comparison
 }
---
+++ Schafkopf.Lib/TrumpfEval.cs
@@ -0,0 +1,96 @@
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace Schafkopf.Lib;
+
+public class TrumpfEval
+{
+    #region Masks
+
+    private const byte SIEBEN = (byte)((byte)CardType.Sieben << 2);
+    private const byte ACHT = (byte)((byte)CardType.Acht << 2);
+    private const byte NEUN = (byte)((byte)CardType.Neun << 2);
+    private const byte UNTER = (byte)((byte)CardType.Unter << 2);
+    private const byte OBER = (byte)((byte)CardType.Ober << 2);
+    private const byte KOENIG = (byte)((byte)CardType.Koenig << 2);
+    private const byte ZEHN = (byte)((byte)CardType.Zehn << 2);
+    private const byte SAU = (byte)((byte)CardType.Sau << 2);
+    private const byte SCHELL = (byte)CardColor.Schell;
+    private const byte HERZ = (byte)CardColor.Herz;
+    private const byte GRAS = (byte)CardColor.Gras;
+    private const byte EICHEL = (byte)CardColor.Eichel;
+
+    private static readonly Vector128<byte> WENZ_TRUMPF_CARDS =
+        Vector128.Create(
+            EICHEL | UNTER, GRAS | UNTER, HERZ | UNTER, SCHELL | UNTER,
+                      0xFF,         0xFF,         0xFF,           0xFF,
+                      0xFF,         0xFF,         0xFF,           0xFF,
+                      0xFF,         0xFF,         0xFF,           0xFF);
+
+    private static readonly Vector128<byte>[] SOLO_TRUMPF_CARDS =
+        new Vector128<byte>[] {
+            Vector128.Create(
+                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
+                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
+                SCHELL |   SAU, SCHELL |   ZEHN, SCHELL | KOENIG, SCHELL |  NEUN,
+                SCHELL |  ACHT, SCHELL | SIEBEN,            0xFF,           0xFF),
+            Vector128.Create(
+                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
+                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
+                  HERZ |   SAU,   HERZ |   ZEHN,   HERZ | KOENIG,   HERZ |  NEUN,
+                  HERZ |  ACHT,   HERZ | SIEBEN,            0xFF,           0xFF),
+            Vector128.Create(
+                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
+                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
+                  GRAS |   SAU,   GRAS |   ZEHN,   GRAS | KOENIG,   GRAS |  NEUN,
+                  GRAS |  ACHT,   GRAS | SIEBEN,            0xFF,           0xFF),
+            Vector128.Create(
+                EICHEL |  OBER,   GRAS |   OBER,   HERZ |   OBER, SCHELL |  OBER,
+                EICHEL | UNTER,   GRAS |  UNTER,   HERZ |  UNTER, SCHELL | UNTER,
+                EICHEL |   SAU, EICHEL |   ZEHN, EICHEL | KOENIG, EICHEL |  NEUN,
+                EICHEL |  ACHT, EICHEL | SIEBEN,            0xFF,           0xFF),
+        };
+
+    private static readonly Vector128<byte> ZERO =
+        Vector128.Create((byte)0);
+    private static readonly Vector128<byte> WENZ_MODE =
+        Vector128.Create((byte)GameMode.Wenz);
+
+    private static readonly Vector128<byte>[] MODE_MASKS =
+        new Vector128<byte>[] {
+            Vector128.Create((byte)GameMode.Weiter),
+            Vector128.Create((byte)GameMode.Sauspiel),
+            Vector128.Create((byte)GameMode.Wenz),
+            Vector128.Create((byte)GameMode.Solo),
+        };
+
+    #endregion Masks
+
+    public TrumpfEval(GameMode mode, CardColor trumpf = CardColor.Herz)
+    {
+        this.mode = mode;
+        this.trumpf = trumpf;
+    }
+
+    private readonly GameMode mode;
+    private readonly CardColor trumpf;
+
+    public bool IsTrumpf(Card card)
+    {
+        var cardVec = Vector128.Create((byte)(card.Id & 0x1F));
+        var modeMask = MODE_MASKS[(byte)mode];
+        var wenzModeMask = Sse2.CompareEqual(modeMask, WENZ_MODE);
+        var soloModeMask = Sse2.CompareEqual(wenzModeMask, ZERO);
+        var soloOrSauspielTrumpf = SOLO_TRUMPF_CARDS[(byte)trumpf];
+
+        var wenzCmp = Sse2.Xor(cardVec, WENZ_TRUMPF_CARDS);
+        var soloCmp = Sse2.Xor(cardVec, soloOrSauspielTrumpf);
+        var wenzMatches = Sse2.And(Sse2.CompareEqual(wenzCmp, ZERO), wenzModeMask);
+        var soloMatches = Sse2.And(Sse2.CompareEqual(soloCmp, ZERO), soloModeMask);
+        var allMatches = Sse2.Or(wenzMatches, soloMatches).AsUInt64();
+        bool isTrumpf = (allMatches.GetElement(0) | allMatches.GetElement(1)) > 0;
+        return isTrumpf;
+    }
+
+    // TODO: use this matching table to compute the rank for card comparison
+}
