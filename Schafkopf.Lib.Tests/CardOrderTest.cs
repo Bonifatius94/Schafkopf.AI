d82833bc640705bcb52422caa7340ea7127c5245
Marco Tröster <marco@troester-gmbh.de>
2022-09-19 13:04:42 +0200

    rework card order test: simplify, test farbe order

--- Schafkopf.Lib.Tests/CardOrderTest.cs
+++ Schafkopf.Lib.Tests/CardOrderTest.cs
@@ -1,277 +1,159 @@
 namespace Schafkopf.Lib.Test;
 
-public class WenzTrumpfCardOrderTest
+public class TrumpfFlagAndCardOrderTest
 {
+    #region Init
+
     private static readonly Random rng = new Random();
 
-    private GameCall newWenz()
+    private static IEnumerable<Card> allTrumpfOfDeckAsc(GameCall call)
+        => (call.Mode == GameMode.Wenz)
+            ? new List<Card>() {
+                new Card(CardType.Unter, CardColor.Schell),
+                new Card(CardType.Unter, CardColor.Herz),
+                new Card(CardType.Unter, CardColor.Gras),
+                new Card(CardType.Unter, CardColor.Eichel),
+            }
+            : new List<Card>() {
+                new Card(CardType.Sieben, call.Trumpf),
+                new Card(CardType.Acht, call.Trumpf),
+                new Card(CardType.Neun, call.Trumpf),
+                new Card(CardType.Koenig, call.Trumpf),
+                new Card(CardType.Zehn, call.Trumpf),
+                new Card(CardType.Sau, call.Trumpf),
+                new Card(CardType.Unter, CardColor.Schell),
+                new Card(CardType.Unter, CardColor.Herz),
+                new Card(CardType.Unter, CardColor.Gras),
+                new Card(CardType.Unter, CardColor.Eichel),
+                new Card(CardType.Ober, CardColor.Schell),
+                new Card(CardType.Ober, CardColor.Herz),
+                new Card(CardType.Ober, CardColor.Gras),
+                new Card(CardType.Ober, CardColor.Eichel),
+            };
+
+    private static IEnumerable<Card> allFarbeOfDeckAsc(GameCall call, CardColor farbe)
+        => (call.Mode == GameMode.Wenz)
+            ? new List<Card>() {
+                new Card(CardType.Sieben, farbe),
+                new Card(CardType.Acht, farbe),
+                new Card(CardType.Neun, farbe),
+                new Card(CardType.Ober, farbe),
+                new Card(CardType.Koenig, farbe),
+                new Card(CardType.Zehn, farbe),
+                new Card(CardType.Sau, farbe),
+            }
+            : new List<Card>() {
+                new Card(CardType.Sieben, farbe),
+                new Card(CardType.Acht, farbe),
+                new Card(CardType.Neun, farbe),
+                new Card(CardType.Koenig, farbe),
+                new Card(CardType.Zehn, farbe),
+                new Card(CardType.Sau, farbe),
+            };
+
+    private static CardsDeck deck = new CardsDeck();
+    private static IEnumerable<Card> allCardsWithMeta(GameCall call)
+        => deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
+
+    private static GameCall newSauspiel()
+    {
+        int playerId = rng.Next(0, 4);
+        var gsuchteSau = (CardColor)rng.Next(0, 4);
+        return GameCall.Sauspiel(playerId, (playerId + 1) % 4, gsuchteSau);
+    }
+
+    private static GameCall newWenz()
     {
         int playerId = rng.Next(0, 4);
         return GameCall.Wenz(playerId);
     }
 
-    [Fact]
-    public void Test_OnlyUnterAreTrumpf_WhenPlayingWenz()
-    {
-        var call = newWenz();
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allCardsWithMeta.Where(x => x.Type == CardType.Unter);
-        var allNonTrumpf = allCardsWithMeta.Except(allTrumpf);
-
-        allTrumpf.Should().Match(x => x.All(card => card.IsTrumpf));
-        allNonTrumpf.Should().Match(x => x.All(card => !card.IsTrumpf));
-    }
-
-    [Fact]
-    public void Test_AnyTrumpfWinsAgainstAnyOtherCard_WhenPlayingWenz()
-    {
-        var call = newWenz();
-        var comp = new CardComparer(call.Mode, call.Trumpf);
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allCardsWithMeta.Where(x => x.Type == CardType.Unter).ToList();
-        var allOtherCards = allCardsWithMeta.Except(allTrumpf).ToList();
-
-        allTrumpf.Should().Match(trumpf => trumpf.All(t =>
-            allOtherCards.All(o => comp.Compare(t, o) > 0)));
-    }
-
-    [Fact]
-    public void Test_TrumpfAreInCorrectOrder_WhenPlayingWenz()
-    {
-        var call = newWenz();
-        var comp = new CardComparer(call.Mode, call.Trumpf);
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-
-        IEnumerable<Card> orderedTrumpf = allCardsWithMeta
-            .Where(x => x.Type == CardType.Unter)
-            .OrderBy(x => x, comp)
-            .ToList();
-
-        IEnumerable<Card> expOrder = new List<Card>() {
-            new Card(CardType.Unter, CardColor.Schell),
-            new Card(CardType.Unter, CardColor.Herz),
-            new Card(CardType.Unter, CardColor.Gras),
-            new Card(CardType.Unter, CardColor.Eichel),
-        };
-
-        orderedTrumpf.Should().BeEquivalentTo(
-            expOrder, options => options.WithStrictOrdering());
-    }
-}
-
-public class SauspielTrumpfCardOrderTest
-{
-    private static readonly Random rng = new Random();
-
-    #region Init
-
-    private IEnumerable<Card> allTrumpfInAscendingOrder(CardColor trumpf)
-        => new List<Card>() {
-            new Card(CardType.Sieben, trumpf),
-            new Card(CardType.Acht, trumpf),
-            new Card(CardType.Neun, trumpf),
-            new Card(CardType.Koenig, trumpf),
-            new Card(CardType.Zehn, trumpf),
-            new Card(CardType.Sau, trumpf),
-            new Card(CardType.Unter, CardColor.Schell),
-            new Card(CardType.Unter, CardColor.Herz),
-            new Card(CardType.Unter, CardColor.Gras),
-            new Card(CardType.Unter, CardColor.Eichel),
-            new Card(CardType.Ober, CardColor.Schell),
-            new Card(CardType.Ober, CardColor.Herz),
-            new Card(CardType.Ober, CardColor.Gras),
-            new Card(CardType.Ober, CardColor.Eichel),
-        };
-
-    private IEnumerable<Card> allTrumpfOfDeck(
-            IEnumerable<Card> origCards,
-            CardColor trumpf)
-    {
-        var trumpfCards = new List<Card>() {
-            new Card(CardType.Sieben, trumpf),
-            new Card(CardType.Acht, trumpf),
-            new Card(CardType.Neun, trumpf),
-            new Card(CardType.Koenig, trumpf),
-            new Card(CardType.Zehn, trumpf),
-            new Card(CardType.Sau, trumpf),
-            new Card(CardType.Unter, CardColor.Schell),
-            new Card(CardType.Unter, CardColor.Herz),
-            new Card(CardType.Unter, CardColor.Gras),
-            new Card(CardType.Unter, CardColor.Eichel),
-            new Card(CardType.Ober, CardColor.Schell),
-            new Card(CardType.Ober, CardColor.Herz),
-            new Card(CardType.Ober, CardColor.Gras),
-            new Card(CardType.Ober, CardColor.Eichel),
-        };
-
-        return origCards.Where(c => trumpfCards.Contains(c)).ToList();
-    }
-
-    private GameCall newSauspiel()
-    {
-        int playerId = rng.Next(0, 4);
-        var gsuchteSau = (CardColor)rng.Next(0, 4);
-        var deck = new CardsDeck();
-        return GameCall.Sauspiel(playerId, playerId + 1, gsuchteSau);
-    }
-
-    #endregion Init
-
-    [Fact]
-    public void Test_HerzAndOberAndUnterAreTrumpf_WhenPlayingSauspiel()
-    {
-        var call = newSauspiel();
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allTrumpfOfDeck(allCardsWithMeta, CardColor.Herz);
-        var notTrumpf = allCardsWithMeta.Except(allTrumpf);
-        allTrumpf.Should().Match(x => x.All(card => card.IsTrumpf));
-        notTrumpf.Should().Match(x => x.All(card => !card.IsTrumpf));
-    }
-
-    [Fact]
-    public void Test_AnyTrumpfWinsAgainstAnyOtherCard_WhenPlayingSauspiel()
-    {
-        var call = newSauspiel();
-        var comp = new CardComparer(call.Mode, call.Trumpf);
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allTrumpfOfDeck(allCardsWithMeta, CardColor.Herz);
-        var notTrumpf = allCardsWithMeta.Except(allTrumpf);
-
-        allTrumpf.Should().Match(trumpf => trumpf.All(t =>
-            notTrumpf.All(o => comp.Compare(t, o) > 0)));
-    }
-
-    [Fact]
-    public void Test_TrumpfAreInCorrectOrder_WhenPlayingSauspiel()
-    {
-        var call = newSauspiel();
-        var comp = new CardComparer(call.Mode, call.Trumpf);
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allTrumpfOfDeck(allCardsWithMeta, CardColor.Herz);
-
-        var orderedTrumpf = allTrumpf
-            .OrderBy(x => x, comp)
-            .ToList();
-
-        var expOrder = allTrumpfInAscendingOrder(CardColor.Herz);
-        orderedTrumpf.Should().BeEquivalentTo(
-            expOrder, options => options.WithStrictOrdering());
-    }
-}
-
-public class SoloTrumpfCardOrderTest
-{
-    private static readonly Random rng = new Random();
-
-    private IEnumerable<Card> allTrumpfInAscendingOrder(CardColor trumpf)
-        => new List<Card>() {
-            new Card(CardType.Sieben, trumpf),
-            new Card(CardType.Acht, trumpf),
-            new Card(CardType.Neun, trumpf),
-            new Card(CardType.Koenig, trumpf),
-            new Card(CardType.Zehn, trumpf),
-            new Card(CardType.Sau, trumpf),
-            new Card(CardType.Unter, CardColor.Schell),
-            new Card(CardType.Unter, CardColor.Herz),
-            new Card(CardType.Unter, CardColor.Gras),
-            new Card(CardType.Unter, CardColor.Eichel),
-            new Card(CardType.Ober, CardColor.Schell),
-            new Card(CardType.Ober, CardColor.Herz),
-            new Card(CardType.Ober, CardColor.Gras),
-            new Card(CardType.Ober, CardColor.Eichel),
-        };
-
-    private IEnumerable<Card> allTrumpfOfDeck(
-            IEnumerable<Card> origCards,
-            CardColor trumpf)
-    {
-        var trumpfCards = new List<Card>() {
-            new Card(CardType.Sieben, trumpf),
-            new Card(CardType.Acht, trumpf),
-            new Card(CardType.Neun, trumpf),
-            new Card(CardType.Koenig, trumpf),
-            new Card(CardType.Zehn, trumpf),
-            new Card(CardType.Sau, trumpf),
-            new Card(CardType.Unter, CardColor.Schell),
-            new Card(CardType.Unter, CardColor.Herz),
-            new Card(CardType.Unter, CardColor.Gras),
-            new Card(CardType.Unter, CardColor.Eichel),
-            new Card(CardType.Ober, CardColor.Schell),
-            new Card(CardType.Ober, CardColor.Herz),
-            new Card(CardType.Ober, CardColor.Gras),
-            new Card(CardType.Ober, CardColor.Eichel),
-        };
-
-        return origCards.Where(c => trumpfCards.Contains(c)).ToList();
-    }
-
-    private GameCall newSolo(CardColor trumpf)
+    private static GameCall newSolo(CardColor trumpf)
     {
         byte playerId = (byte)rng.Next(0, 4);
         return GameCall.Solo(playerId, trumpf);
     }
 
+    private static IEnumerable<CardColor> allFarben
+        => new List<CardColor>() {
+            CardColor.Schell, CardColor.Herz,
+            CardColor.Gras, CardColor.Eichel
+        };
+
+    private static IEnumerable<GameCall> callsToTest
+        => new List<GameCall>() { newSauspiel(), newWenz() }
+            .Union(allFarben.Select(t => newSolo(t)));
+
+    private static IEnumerable<CardColor> farbenForCall(GameCall call)
+        => call.Mode == GameMode.Wenz ? allFarben
+            : allFarben.Except(new List<CardColor>() { call.Trumpf });
+
+    public static IEnumerable<object[]> callsXExpTrumpfXNotExpTrumpf
+        => callsToTest.Select(call => new object[] {
+            call,
+            allTrumpfOfDeckAsc(call)
+                .Select(c => allCardsWithMeta(call).First(m => m == c)).ToArray(),
+            CardsDeck.AllCards.Except(allTrumpfOfDeckAsc(call))
+                .Select(c => allCardsWithMeta(call).First(m => m == c)).ToArray()
+        });
+
+    public static IEnumerable<object[]> callsXAllFarbeAsc
+        => callsToTest.SelectMany(call => 
+            farbenForCall(call).Select(farbe => new object[] {
+                call,
+                allFarbeOfDeckAsc(call, farbe)
+                    .Select(c => allCardsWithMeta(call).First(m => m == c)).ToArray(),
+            })
+        );
+
+    #endregion Init
+
     [Theory]
-    [InlineData(CardColor.Schell)]
-    [InlineData(CardColor.Herz)]
-    [InlineData(CardColor.Gras)]
-    [InlineData(CardColor.Eichel)]
-    public void Test_TrumpfColorAndOberAndUnterAreTrumpf_WhenPlayingSolo(CardColor trumpf)
+    [MemberData(nameof(callsXExpTrumpfXNotExpTrumpf))]
+    public void Test_IsTrumpfOnlyTrueForCardsThatAreTrumpf_GivenTheGameMode(
+        GameCall call, IEnumerable<Card> allTrumpf, IEnumerable<Card> allNonTrumpf)
     {
-        var call = newSolo(trumpf);
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allTrumpfOfDeck(allCardsWithMeta, trumpf);
-        var notTrumpf = allCardsWithMeta.Except(allTrumpf);
         allTrumpf.Should().Match(x => x.All(card => card.IsTrumpf));
-        notTrumpf.Should().Match(x => x.All(card => !card.IsTrumpf));
+        allNonTrumpf.Should().Match(x => x.All(card => !card.IsTrumpf));
     }
 
     [Theory]
-    [InlineData(CardColor.Schell)]
-    [InlineData(CardColor.Herz)]
-    [InlineData(CardColor.Gras)]
-    [InlineData(CardColor.Eichel)]
-    public void Test_AnyTrumpfWinsAgainstAnyOtherCard_WhenPlayingSolo(CardColor trumpf)
+    [MemberData(nameof(callsXExpTrumpfXNotExpTrumpf))]
+    public void Test_AnyTrumpfWinsAgainstAnyOtherCard_GivenTheGameMode(
+        GameCall call, IEnumerable<Card> allTrumpf, IEnumerable<Card> allNonTrumpf)
     {
-        var call = newSolo(trumpf);
         var comp = new CardComparer(call.Mode, call.Trumpf);
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allTrumpfOfDeck(allCardsWithMeta, trumpf);
-        var notTrumpf = allCardsWithMeta.Except(allTrumpf);
-
         allTrumpf.Should().Match(trumpf => trumpf.All(t =>
-            notTrumpf.All(o => comp.Compare(t, o) > 0)));
+            allNonTrumpf.All(o => comp.Compare(t, o) > 0)));
     }
 
     [Theory]
-    [InlineData(CardColor.Schell)]
-    [InlineData(CardColor.Herz)]
-    [InlineData(CardColor.Gras)]
-    [InlineData(CardColor.Eichel)]
-    public void Test_TrumpfAreInCorrectOrder_WhenPlayingSolo(CardColor trumpf)
+    [MemberData(nameof(callsXExpTrumpfXNotExpTrumpf))]
+    public void Test_TrumpfAreInCorrectOrder_GivenTheGameMode(
+        GameCall call, IEnumerable<Card> allTrumpfAsc, IEnumerable<Card> allNonTrumpf)
     {
-        var call = newSolo(trumpf);
         var comp = new CardComparer(call.Mode, call.Trumpf);
-        var deck = new CardsDeck();
-        var allCardsWithMeta = deck.SelectMany(h => h.CacheTrumpf(call.IsTrumpf)).ToArray();
-        var allTrumpf = allTrumpfOfDeck(allCardsWithMeta, trumpf);
 
-        var orderedTrumpf = allTrumpf
+        var orderedTrumpf = allTrumpfAsc
             .OrderBy(x => x, comp)
             .ToList();
 
-        var expOrder = allTrumpfInAscendingOrder(trumpf);
         orderedTrumpf.Should().BeEquivalentTo(
-            expOrder, options => options.WithStrictOrdering());
+            allTrumpfAsc, options => options.WithStrictOrdering());
+    }
+
+    [Theory]
+    [MemberData(nameof(callsXAllFarbeAsc))]
+    public void Test_FarbenAreInCorrectOrder_GivenTheGameMode(
+        GameCall call, IEnumerable<Card> callsXAllFarbeAsc)
+    {
+        var comp = new CardComparer(call.Mode, call.Trumpf);
+
+        var orderedTrumpf = callsXAllFarbeAsc
+            .OrderBy(x => x, comp)
+            .ToList();
+
+        orderedTrumpf.Should().BeEquivalentTo(
+            callsXAllFarbeAsc, options => options.WithStrictOrdering());
     }
 }
-
-// TODO: add test for Farbe order in all game modes
