81207f7df23afb214609478ca2a532f2b59becd6
Marco Tröster <marco@troester-gmbh.de>
2022-09-24 14:01:35 +0200

    only run etw profiler on windows

--- Schafkopf.Lib.Benchmarks/GameSessionBenchmarks.cs
+++ Schafkopf.Lib.Benchmarks/GameSessionBenchmarks.cs
@@ -3,7 +3,9 @@ using BenchmarkDotNet.Diagnostics.Windows.Configs;
 namespace Schafkopf.Lib.Benchmarks;
 
 [MemoryDiagnoser(false)]
+#if Windows
 [EtwProfiler(performExtraBenchmarksRun: false)]
+#endif
 public class GameSessionBenchmark
 {
     // [Params(100, 10000, 1000000)]
