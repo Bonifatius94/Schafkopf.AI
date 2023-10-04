65ad72bc98fd8fa2ad4f2cd82f818cd37f0fe25d
Marco Tröster <marco@troester-gmbh.de>
2023-10-05 01:26:43 +0200

    clean up

--- Schafkopf.Training/python/training/train_model.py
+++
@@ -1,5 +0,0 @@
-
-# 1) read in the training data and convert it to a TensorFlow dataset
-# 2) load an existing TensorFlow model
-# 3) run the training for some period of time
-# 4) export the model again
