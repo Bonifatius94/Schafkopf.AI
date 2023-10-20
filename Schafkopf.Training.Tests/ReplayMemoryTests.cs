4d6e7afeb24ec6b5a411178629a3d8f7ea20e847
Marco Tröster <marco@troester-gmbh.de>
2023-10-20 09:58:57 +0200

    extend replay buffer, add tests

---
+++ Schafkopf.Training.Tests/ReplayMemoryTests.cs
@@ -0,0 +1,47 @@
+namespace Schafkopf.Training.Tests;
+
+public class ReplayMemoryTests
+{
+    [Fact]
+    public void Test_CanFillCacheUntilOverflow()
+    {
+        var memory = new ReplayMemory(100);
+
+        for (int i = 0; i < 50; i++)
+            memory.Append(new SarsExp());
+
+        Assert.Equal(50, memory.Size);
+
+        for (int i = 0; i < 50; i++)
+            memory.Append(new SarsExp());
+
+        Assert.Equal(100, memory.Size);
+    }
+
+    [Fact]
+    public void Test_CanInsertIntoOverflowingCache()
+    {
+        var memory = new ReplayMemory(100);
+
+        for (int i = 0; i < 200; i++)
+            memory.Append(new SarsExp());
+
+        Assert.Equal(100, memory.Size);
+    }
+
+    [Fact]
+    public void Test_CanReplaceOverflowingDataWithNewData()
+    {
+        var memory = new ReplayMemory(100);
+        var overflowingData = Enumerable.Range(0, 50).Select(x => new SarsExp()).ToArray();
+        var insertedData = Enumerable.Range(0, 100).Select(x => new SarsExp()).ToArray();
+
+        foreach (var exp in overflowingData)
+            memory.Append(exp);
+        foreach (var exp in insertedData)
+            memory.Append(exp);
+
+        Assert.True(overflowingData.All(exp => !memory.Contains(exp)));
+        Assert.True(insertedData.All(exp => memory.Contains(exp)));
+    }
+}
--- Schafkopf.Training/ReplayMemory.cs
+++ Schafkopf.Training/ReplayMemory.cs
@@ -1,27 +1,79 @@
 namespace Schafkopf.Training;
 
-public class SarsExp
+public struct SarsExp
 {
-    public GameState StateBefore { get; init; }
-    public GameState StateAfter { get; init; }
-    // TODO: think about whether this is a good action representation
-    public int Action { get; init; }
-    public float Reward { get; init; }
-    public bool IsTerminal { get; init; }
+    public SarsExp() { }
+
+    public GameState StateBefore = new GameState();
+    public GameState StateAfter = new GameState();
+    public GameAction Action = new GameAction();
+    public double Reward = 0.0;
+    public bool IsTerminal = false;
 }
 
 public class ReplayMemory
 {
-    private const int memorySize = 10000;
+    private static readonly Random rng = new Random();
 
+    public ReplayMemory(int size)
+    {
+        totalSize = size;
+        memory = new SarsExp[totalSize];
+    }
+
+    private int totalSize;
     private bool isFilled = false;
-    private int i = 0;
-    private SarsExp[] experience = new SarsExp[memorySize];
+    private bool overflow = false;
+    private int insertPos = 0;
+    private SarsExp[] memory;
+
+    public int Size => isFilled ? totalSize : insertPos;
 
     public void Append(SarsExp exp)
     {
-        experience[i++] = exp;
-        isFilled = isFilled || i == memorySize;
-        i %= memorySize;
+        memory[insertPos++] = exp;
+        overflow = insertPos == totalSize;
+        isFilled = isFilled || overflow;
+        insertPos %= totalSize;
     }
+
+    public void AppendBatched(SarsExp[] exps)
+    {
+        bool overflow = false;
+        foreach (var exp in exps)
+        {
+            Append(exp);
+            overflow |= this.overflow;
+        }
+        this.overflow = overflow;
+    }
+
+    public void SampleRandom(SarsExp[] cache)
+    {
+        int maxId = isFilled ? totalSize : insertPos;
+        for (int i = 0; i < cache.Length; i++)
+            cache[i] = memory[rng.Next() % maxId];
+    }
+
+    public void SampleBatched(SarsExp[] cache)
+    {
+        int maxId = isFilled ? totalSize : insertPos;
+        int offset = rng.Next(0, totalSize - cache.Length);
+        for (int i = 0; i < cache.Length; i++)
+            cache[i] = memory[(offset + i) % maxId];
+    }
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < totalSize; i++)
+        {
+            int j = rng.Next(i, totalSize);
+            var temp = memory[j];
+            memory[j] = memory[i];
+            memory[i] = temp;
+        }
+    }
+
+    public bool Contains(SarsExp exp)
+        => memory.Contains(exp);
 }
