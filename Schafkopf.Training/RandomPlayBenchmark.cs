b27b50a7a714c10b6042a13f59d6e980e042d067
Marco Tröster <marco@troester-gmbh.de>
2023-11-06 22:57:52 +0100

    add random play benchmark

---
+++ Schafkopf.Training/RandomPlayBenchmark.cs
@@ -0,0 +1,32 @@
+namespace Schafkopf.Training;
+
+public class RandomPlayBenchmark
+{
+    public void Benchmark(ISchafkopfAIAgent agentToEval, int epochs = 10_000)
+    {
+        var players = new Player[] {
+            new Player(0, agentToEval),
+            new Player(1, new RandomAgent()),
+            new Player(2, new RandomAgent()),
+            new Player(3, new RandomAgent())
+        };
+        var table = new Table(
+            players[0], players[1],
+            players[2], players[3]);
+        var deck = new CardsDeck();
+        var session = new GameSession(table, deck);
+
+        int wins = 0;
+        for (int i = 0; i < epochs; i++)
+        {
+            var log = session.ProcessGame();
+            var eval = new GameScoreEvaluation(log);
+            bool isCaller = log.Meta.CallerIds.Contains(0);
+            bool isWin = !eval.DidCallerWin ^ isCaller;
+            wins += isWin ? 1 : 0;
+        }
+
+        double winRate = (double)wins / epochs;
+        Console.WriteLine($"agent scored a win rate of {winRate}!");
+    }
+}
